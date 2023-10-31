46c1ffd3c53ffe3884fa74dacb660c2110457960
ares0204 <bt.thanhphuc@gmail.com>
2023-10-31 14:56:38 +0700

    update

---
+++ P12T/Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace P12T.Areas.Admin.Controllers
+{
+    public class HomeAdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
